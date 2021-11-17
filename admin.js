c0d849d7f36d6cd4e51ac14dc1913dd5cd9f23c6
Ramahadi Jan <moheanerj@gmail.com>
2021-11-17 14:31:38 +0200

    finalized updates

--- admin.js
+++ admin.js
@@ -2,12 +2,22 @@ module.exports = function admin() {
     let user = 'show';
     let post= 'hidden';
     let update= 'hidden';
+    let register = 'hidden'
+    let update_in = 'hidden'
     
-    
+    function resetadmin() {
+            user = 'show';
+         post= 'hidden';
+         update= 'hidden';
+         register = 'hidden'
+         update_in = 'hidden'
+    }
     function showuser() {
         user = 'show'
         post = 'hidden'
         update = 'hidden'
+        register = 'hidden'
+        update_in = 'hidden'
     }
 
     function returnuser() {
@@ -17,6 +27,8 @@ module.exports = function admin() {
         user = 'hidden'
         post = 'show'
         update = 'hidden'
+        register = 'hidden'
+        update_in = 'hidden'
     }
 
     function returnpost() {
@@ -27,18 +39,49 @@ module.exports = function admin() {
         user = 'hidden'
         post = 'hidden'
         update = 'show'
+        register = 'hidden'
+        update_in = 'hidden'
     }
 
     function returnupdate() {
         return update
     }
+    function showregister() {
+        
+        user = 'show'
+        post = 'hidden'
+        update = 'hidden'
+        update_in = 'hidden'
+        register = 'show'
+    }
+    function returnregister() {
+        return register
+    }
+    function showupdate_input() {
+        user = 'hidden'
+        post = 'hidden'
+        update = 'show'
+        update_in = 'show'
+        register = 'hidden'
+    }
+    function returnupdate_input() {
+        return update_in
+    }
     
     return{
         showuser,
         showpost,
+
         showupdate,
         returnuser,
+
         returnpost,
-        returnupdate
+        returnupdate,
+
+        showregister,
+        returnregister,
+        showupdate_input,
+        returnupdate_input,
+        resetadmin
     }
 }
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-const MemoryStore = require('memorystore')(session)
+// const MemoryStore = require('memorystore')(session)
 const { Client } = require("pg");
 const ad = require("./admin");
 
 const admin = ad();
 
 const client = new Client({
-  user: "tzrshwebdhrysp",
-  password: "eba676700c384bc03e60a7e05ae371e30a43d6f0ea53c02cc5eae8f383b90feb",
-  host: "ec2-52-54-237-144.compute-1.amazonaws.com",
-  database: "dfpvabsnh8th5e",
+  user: "postgres",
+  password: "3980",
+  host: "localhost",
+  database: "Intellihomes",
 });
 
 const app = express();
@@ -22,10 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    cookie: { maxAge: 86400000 },
-    store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
-    }),
+    // cookie: { maxAge: 86400000 },
+    // store: new MemoryStore({
+    //   checkPeriod: 86400000 // prune expired entries every 24h
+    // }),
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
@@ -41,12 +41,15 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.get("/", async function (req, res) {
+    userid = req.session.userid
   username = req.session.username;
+  console.log(userid+username)
   if (!req.session.username) {
     res.redirect("/login");
   } else {
     results = await client.query("SELECT * FROM updatestb")
-
+    results3 = await client.query("SELECT userid FROM userstb WHERE email = $1", [req.session.username])
+    console.log(results3.rows[0].userid)
       res.render("home", {
         results: results.rows,
       
@@ -105,7 +108,8 @@ app.get("/login", function (req, res) {
 
 app.post("/login", async function (req, res) {
   const { username, password, userType } = req.body;
-  client.connect(
+  client.connect();
+  
     await client.query(
       "SELECT * FROM userstb WHERE email = $1 AND password = $2",
       [username, password],
@@ -116,11 +120,12 @@ app.post("/login", async function (req, res) {
           var Data = results.rows;
           console.log(Data);
           Data.forEach((row) => {
-            (email = row.email), (passwords = row.password), (role = row.userole), (firstname = row.firstname);
+            (email = row.email), (passwords = row.password), (role = row.userole), (firstname = row.firstname), (userid = row.userid);
           });
           if (req.body.userType && req.body.username && req.body.password) {
             req.session.role = req.body.userType;
             req.session.username = req.body.username;
+            req.session.userid = req.body.userid;
             req.session.password = req.body.password;
             console.log(
                 email + req.body.username + req.body.password
@@ -165,7 +170,7 @@ app.post("/login", async function (req, res) {
         }
       }
     )
-  );
+  
 });
 
 app.get("/logout", function (req, res) {
@@ -239,16 +244,24 @@ app.get("/admin", async function (req, res) {
       
     results = await client.query("SELECT * FROM userstb")
     results2 = await client.query("SELECT * FROM updatestb")
-    users_control = admin.returnuser();
+        
+        users_control = admin.returnuser();
+        update_input_control = admin.returnupdate_input();
         updates_control = admin.returnupdate();
         posts_control = admin.returnpost();
+        register_control = admin.returnregister();
+
         console.log(users_control);
         res.render("admin/index", {
             results: results.rows,
             results2: results2.rows,
+
           users_control: users_control,
           posts_control: posts_control,
           updates_control: updates_control,
+          register_control: register_control,
+          update_input_control: update_input_control,
+          
         });
 
 
@@ -270,8 +283,26 @@ app.post("/admin", function (req, res) {
     console.log(btn);
   }
 
-  res.redirect("admin");
+  res.redirect("/admin");
 });
+
+app.post('/register_control', function (req,res) {
+    admin.showregister()
+    res.redirect('/admin')
+})
+app.post('/updates_control', function (req,res) {
+    admin.showupdate_input()
+    res.redirect('/admin')
+})
+
+app.post('/close_reg', function (req,res) {
+    admin.resetadmin()
+    res.redirect('/admin')
+})
+app.post('/close_upd', function (req,res) {
+    admin.showupdate()
+    res.redirect('/admin')
+})
 
 
 app.post('/post_update', async function (req, res) {
@@ -300,6 +331,12 @@ app.post('/post_update', async function (req, res) {
           }
         )
       );
+})
+
+app.post('/delete_update/:update_id', async function (req,res) {
+    insert_value = req.params.update_id
+    results3 = await client.query("DELETE FROM userstb WHERE update_id = $1", [insert_value])
+    res.redirect('/admin')
 })
 app.listen(PORT, function () {
   console.log("app running at port 3017");
