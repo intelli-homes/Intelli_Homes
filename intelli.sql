79a95eebd2938fe6c340eb385d566510b77a47d4
Ramahadi Jan <moheanerj@gmail.com>
2021-11-24 05:24:24 +0200

    updated db file

--- intelli.sql
+++ intelli.sql
@@ -1,4 +1,4 @@
-CREATE TABLE userstb( userid SERIAL, firstname VARCHAR(32), lastname VARCHAR(32), 
+CREATE TABLE userstb( userid SERIAL PRIMARY KEY, firstname VARCHAR(32), lastname VARCHAR(32), 
 				   DOB VARCHAR(64), gender VARCHAR(32), password text, password2 text,
 				   email text unique, phone text, userole text, avator bytea);
 				
@@ -7,16 +7,29 @@ INSERT INTO userstb( firstname, lastname, DOB, gender, password, password2, emai
 VALUES('Ramahadi', 'Moheane', ' 3 may', 'male', '1234', '1234', 'moheanerj@gmail', '0788356810', 'Admin', 'C:/Users/Lenovo-User/Pictures/Screenshots/1.jpg'); 
 
 
-CREATE TABLE updatestb( updateid SERIAL, tittle VARCHAR(32), post_content VARCHAR(2000), created_on timestamp without time zone DEFAULT now(), image bytea);
+CREATE TABLE updatestb( updateid SERIAL PRIMARY KEY, tittle VARCHAR(32), post_content VARCHAR(2000), created_on timestamp without time zone DEFAULT now(), image bytea);
 				
 		
 INSERT INTO updatestb(tittle, post_content, image) 
 VALUES('Cops kills a crimimnal', ' vehicle and three armed men got out and approached the vehicle.', 'C:/Users/Lenovo-User/Pictures/Screenshots/2.jpg'); 
 
 
+CREATE TABLE poststb( postid SERIAL PRIMARY KEY, userid  INTEGER REFERENCES userstb(userid), tittle VARCHAR(50), 
+				    postmedia bytea, postcontent VARCHAR(6400));
+				
+				
+INSERT INTO poststb values (1,1,'Crimes in Windburg', 'C:/Users/Lenovo-User/Pictures/Screenshots/1.jpg', ' suspect appeared in the Randburg Magistrate’s Court on 29 June after detectives charged him with fraud.
+
+According to Sandton Police Station spokesperson Captain Granville Meyer, the 47-year-old male suspect allegedly tried to purchase a cell phone with a fraudulent shopping card at a retail store in Sandton on 27 June.' ); 
 
 
 
-select * from updatestb
 
 select * from userstb
+
+select * from poststb
+
+select userstb.firstname, userstb.lastname, poststb.tittle, poststb.postcontent
+from userstb
+INNER JOIN
+poststb ON userstb.userid = poststb.userid
