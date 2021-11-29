336b0bcdb24d39c2427cb318751f2886e392a09d
Ramahadi Jan <moheanerj@gmail.com>
2021-11-29 16:00:32 +0200

    redirected to admin after post updated

--- index.js
+++ index.js
@@ -148,7 +148,15 @@ open({
       }
     }
   });
-
+  
+  app.get("/access", function (req, res) {
+    if (!req.session.username) {
+      res.redirect("/login");
+    } else {
+      res.render("door-access");
+    }
+  });
+  
   app.get("/reports", function (req, res) {
     if (!req.session.username) {
       res.redirect("/login");
