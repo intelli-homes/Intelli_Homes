3d550639f75553fe5ce947146fb627916582a3c2
Ramahadi Jan <moheanerj@gmail.com>
2021-11-30 13:49:28 +0200

    removed notifications

--- public/test.js
+++ public/test.js
@@ -4,14 +4,16 @@ const MODEL_URL = '/weights'
 
 
 async function detectface() {
-//  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-// await faceapi.loadFaceLandmarkModel(MODEL_URL)
-// await faceapi.loadFaceRecognitionModel(MODEL_URL)
+ await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+await faceapi.loadFaceLandmarkModel(MODEL_URL)
+await faceapi.loadFaceRecognitionModel(MODEL_URL)
     console.log('1')
-    const video = await faceapi.fetchImage('/images/cam2.jpg')
+    const video = await faceapi.fetchImage('/images/cr3.jpg')
     console.log('12')
+    console.log(video)
     detections = await faceapi.detectAllFaces(video)
     console.log('123')
+    console.log(detections)
     return detections 
 }
 function df() {
